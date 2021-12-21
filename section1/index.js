d4a2ecfb86f26b10f08259269f8d94fe39e75a9b
佐藤拓磨 <>
2021-12-21 15:52:04 +0900

    [Add] section1

---
+++ section1/index.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var hello = 'hello';
+console.log(hello);
+function add(a, b) {
+    return a + b;
+}
---
+++ section1/index.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+
+let hello: string = 'hello';
+console.log(hello);
+
+function add(a: number, b: number): number {
+    return a + b;
+}
